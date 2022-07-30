b3f697278e0683c966d8674d1696b6f0a728abba
Luis <garcialh@gmail.com>
2022-07-30 11:58:34 -0400

    terminando detalles de validacion y diseño.

--- js/encriptar.js
+++ js/encriptar.js
@@ -10,40 +10,39 @@ botonenc.addEventListener("click",function(event){
     var tex_arr= Array.from(texto);
     var tex_enc="";
     var tex_con="";
+    var valida=false;
+    var mensaje="";
 
-    tex_arr.forEach(function(letra){
+    formulario.textoe.value=tex_enc;
 
-        var letra_eva=letra;
+    if (validatexto(texto)){
+        tex_arr.forEach(function(letra){
 
-        console.log(letra_eva);
+            var letra_eva=letra;            
+            switch (letra_eva) {
+                case "e":
+                    tex_con="enter";
+                    break;
+                case "i":
+                    tex_con="imes";
+                    break;
+                case "a":
+                    tex_con="ai";
+                    break;
+                case "o":
+                    tex_con="ober";
+                    break;
+                case "u":
+                    tex_con="ufat";
+                    break;
+                default:
+                    tex_con=letra_eva;
+                    break;
+            }
+            tex_enc = tex_enc + tex_con;
+        });
+    };
 
-        switch (letra_eva) {
-            case "e":
-                tex_con="enter";
-                break;
-            case "i":
-                tex_con="imes";
-                break;
-            case "a":
-                tex_con="ai";
-                break;
-            case "o":
-                tex_con="ober";
-                break;
-            case "u":
-                tex_con="ufat";
-                break;
-            default:
-                tex_con=letra_eva;
-                break;
-        }
-
-        tex_enc = tex_enc + tex_con;
-        console.log(letra_eva+" - "+tex_con);
-
-    });
-
-    console.log(tex_arr);
     formulario.textoe.value=tex_enc;
 })
 
